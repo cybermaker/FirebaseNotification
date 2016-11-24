f96a430a9a7d55746257c39fdcd5fad9cc571e1f
cybermaker <suppanyu2538@gmail.com>
2016-11-24 11:11:11 +0700

    2

--- app/src/main/java/com/egco428/firebasenotification/Main.java
+++ app/src/main/java/com/egco428/firebasenotification/Main.java
@@ -3,11 +3,17 @@ package com.egco428.firebasenotification;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
+
 public class Main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        FirebaseInstanceId.getInstance();
+        FirebaseMessaging.getInstance();
     }
 }
--- app/src/main/java/com/egco428/firebasenotification/MyFirebaseMessagingService.java
+++ app/src/main/java/com/egco428/firebasenotification/MyFirebaseMessagingService.java
@@ -3,5 +3,23 @@ package com.egco428.firebasenotification;
 /**
  * Created by 6272user on 11/24/2016 AD.
  */
-public class MyFirebaseMessagingService {
+import android.util.Log;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    public void onMessageReceived(RemoteMessage remoteMessage){
+
+        for(String k:remoteMessage.getData().keySet()){
+            String s = remoteMessage.getData().get(k);
+            Log.i("Result","onMessageReceived"+s);
+        }
+        if(remoteMessage.getNotification() != null ){
+            String s = remoteMessage.getNotification().getBody();
+            Log.i("Result","onMessageReceived"+s);
+        }
+    }
 }
+
